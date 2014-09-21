8829a5bd71d499c7a8890af87b70d1ff0699434b
robertmaury <robert.maury@puppetlabs.com>
2014-09-21 16:46:47 -0400

    First Commit

---
+++ manifests/openssh.pp
@@ -0,0 +1,23 @@
+class openssh {
+  
+  package {'openssh-server':
+    ensure => '5.3p1-94',
+  }
+
+  file {'/etc/ssh/sshd_config':
+    ensure  => file,
+    owner   => root,
+    group   => root,
+    mode    => 0644,
+    #source  => 'puppet:///modules/openssh/sshd_config',
+    content => template("openssh/sshd_config.erb")
+    require => Package['openssh-server']
+  }
+
+  service {'sshd':
+    ensure    => running,
+    enable    => true,
+    subscribe => File['sshd_config']
+  }
+
+}
