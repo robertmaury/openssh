041e3b04467c5a9dfe1e51c7033cff4046707bd7
Robert Maury <robert.maury@puppetlabs.com>
2014-12-09 14:52:12 -0500

    I'm a terribly sysadmin

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,16 +1,14 @@
-# Sample class to install and manage an openssh server
-
 class openssh {
   
   package {'openssh-server':
-    ensure => '5.3p1-94',
+    ensure => 'latest',
   }
 
   file {'/etc/ssh/sshd_config':
     ensure  => file,
     owner   => root,
     group   => root,
-    mode    => '0644',
+    mode    => 644,
     source  => 'puppet:///modules/openssh/sshd_config',
     #content => template("openssh/sshd_config.erb")
     require => Package['openssh-server']
